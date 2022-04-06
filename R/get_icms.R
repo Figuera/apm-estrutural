c011a881b57b8b6af79c17f3326cfba98933692b
Figuera <apoema.asa@gmail.com>
2022-04-05 22:13:07 -0400

    New no share method

--- R/get_icms.R
+++ R/get_icms.R
@@ -1,13 +1,13 @@
 get_icms <- function() {
-  tb_ckan <- ckanr::resource_show(id="48f9698b-fa39-4337-82ca-df5c0b02d866",url="https://dados.gov.br/")
+  tb_ckan <- ckanr::resource_show(id = "48f9698b-fa39-4337-82ca-df5c0b02d866", url = "https://dados.gov.br/")
 
-  URL_add <- tb_ckan$url
+  url_add <- tb_ckan$url
 
   tmp <- tempfile(fileext = ".xls")
-  download.file(URL_add,mode = "wb", destfile = tmp, extra = "-k", method = "curl")
+  download.file(url_add, mode = "wb", destfile = tmp, extra = "-k", method = "curl")
 
   icms <- readxl::read_xls(tmp, sheet = 2, skip = 1)
-  icms <- icms[,c(1,2,4,5,22)]
+  icms <- icms[, c(1, 2, 4, 5, 22)]
 
   icms %>% dplyr::transmute(
       uf    = factor(icms$id_uf, levels = unique(icms$id_uf), labels = unique(icms$ESTADO)),
--- R/process_revenue.R
+++ R/process_revenue.R
@@ -1,56 +1,25 @@
 library(rtn)
 library(tidyverse)
-library(dplyr)
 library(lubridate)
 
 process_revenue <- function(value) {
-  receitas  <- rtn::get_revenue_data()
-  trt_share <- readr::read_rds("data/trt_share.rds")
-  tm_share  <- readr::read_rds("data/tm_share.rds")
-
-  trt_share <- trt_share %>%
-    mutate(month = month(date)) %>%
-    group_by(month) %>%
-    summarise(share = mean(share))
-  tm_share <- tm_share %>%
-    mutate(month = month(date)) %>%
-    group_by(month) %>%
-    summarise(pis_share = mean(pis), cofins_share = mean(cofins))
-
-  receitas %<>% mutate(
-    month        = month(Data),
+  receitas  <- rtn::get_revenue_data() %>%
+    mutate(month        = month(Data),
     periodo      = str_c(year(Data), "_", quarter(Data)),
     rubrica_code = str_extract(Rubrica, "[\\d,\\.]*"),
     rubrica_name = str_extract(Rubrica, "[A-Z,a-z].*$"),
-    valor = {{value}})
+    valor        = valor_historico)#{{value}})
 
-
-  # Split IRPF from work related income (trt) and Capital related income (TGP)
-  trt_new_lines        <- rtn::rev_new_lines(receitas, "1.1.03.1", trt_share, 1)
-  tgc_new_lines        <- rtn::rev_new_lines(receitas, "1.1.03.1", trt_share, 2)
-  # Split COFINS from Importation-related or not
-  tm_cofins_new_lines  <- rtn::rev_new_lines(receitas, "1.1.05", transmute(tm_share, month, share = cofins_share), 1)
-  ti_cofins_new_lines  <- rtn::rev_new_lines(receitas, "1.1.05", transmute(tm_share, month, share = cofins_share), 2)
-  # Split PIS from Importation-related or not
-  tm_pis_new_lines     <- rtn::rev_new_lines(receitas, "1.1.06", transmute(tm_share, month, share = pis_share),    1)
-  ti_pis_new_lines     <- rtn::rev_new_lines(receitas, "1.1.06", transmute(tm_share, month, share = pis_share),    2)
-  # Add new lines to data.frame
-  receitas <- rbind(receitas, trt_new_lines, tgc_new_lines, tm_cofins_new_lines,
-    tm_pis_new_lines, ti_cofins_new_lines, ti_pis_new_lines)
+  calc(receita)
 
   # Create Revenue Groups
-  trt  <- rtn::get_rubricas(filter(receitas, Data >= as.Date("2000-1-1")), c("1.1.03.1-1", "1.1.03.3.1"), "trt")
-  tfp  <- rtn::get_rubricas(receitas, c("1.4.6", "1.3"), "tfp")
-  trc  <- rtn::get_rubricas(receitas, c("1.1.03.2", "1.1.07"), "trc")
-  tgc  <- rtn::get_rubricas(receitas %>% filter(Data >= as.Date("2000-1-1")), c("1.1.03.1-2", "1.1.03.3.2"), "tgc")
-  tran <- rtn::get_rubricas(receitas, c("2.1", "2.3", "2.4"), "tran")
-
-  ti_1  <- rtn::get_rubricas(filter(receitas, Data < as.Date("2004-05-1")), c("1.1.05", "1.1.06"), "ti")
-  ti_2  <- rtn::get_rubricas(filter(receitas, Data >= as.Date("2004-05-1")), c("1.1.05-2", "1.1.06-2"), "ti")
-  ti    <- rbind(ti_1, ti_2) %>% group_by(periodo) %>% summarise(ti = sum(ti))
-  tm_1  <- rtn::get_rubricas(filter(receitas, Data < as.Date("2004-05-1")), c("1.1.01"), "tm")
-  tm_2  <- rtn::get_rubricas(filter(receitas, Data >= as.Date("2004-05-1")), c("1.1.01", "1.1.05-1", "1.1.06-1"), "tm")
-  tm    <- rbind(tm_1, tm_2) %>% group_by(periodo) %>% summarise(tm = sum(tm))
+  trt  <- get_rubricas(receitas, c("1.1.03.1", "1.1.03.3.1"), "trt")
+  tfp  <- get_rubricas(receitas, c("1.4.6", "1.3"),           "tfp")
+  trc  <- get_rubricas(receitas, c("1.1.03.2", "1.1.07"),     "trc")
+  tgc  <- get_rubricas(receitas, c("1.1.03.3.2"),             "tgc")
+  tran <- get_rubricas(receitas, c("2.1", "2.3", "2.4"),      "tran")
+  ti   <- get_rubricas(receitas, c("1.1.05", "1.1.06"),       "ti")
+  tm   <- get_rubricas(receitas, c("1.1.01"),                 "tm")
 
   # Get ICMS data on the correct format
   icms <- get_icms() %>%
